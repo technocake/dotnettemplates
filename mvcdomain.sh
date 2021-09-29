82258fa5f31f2ca5fcc9f694fc0e2af11b4e7967
technocake <robin.garen@gmail.com>
2021-09-29 11:12:31 +0200

    add xunit test to template

--- mvcdomain.sh
+++ mvcdomain.sh
@@ -7,6 +7,7 @@ DESCRIPTION="Generate a mvc web project and a domain (netstandard) classlibrary.
 NAME=$1
 WEBNAME=web
 DOMAINNAME=$NAME.Domain
+TESTNAME=$NAME.Tests
 
 if [ -z $NAME ]; then
 	echo "\n\t Usage: $(basename $0) <name>\n" 
@@ -24,10 +25,17 @@ dotnet new sln -n $NAME
 # Create projects
 dotnet new mvc -o $WEBNAME
 dotnet new classlib -f netstandard2.1 -o $DOMAINNAME
+dotnet new xunit $TESTNAME
 
 # Add projects to solution
 dotnet sln add $WEBNAME/$WEBNAME.csproj
 dotnet sln add $DOMAINNAME/$DOMAINNAME.csproj
+dotnet sln add $TESTNAME/$TESTNAME.csproj
 
 # Set up project reference
 dotnet add $WEBNAME/$WEBNAME.csproj reference $DOMAINNAME/$DOMAINNAME.csproj
+dotnet add $TESTNAME/$TESTNAME.csproj reference $WEBNAME/$WEBNAME.csproj
+
+# Domain project structure
+mkdir $DOMAINNAME/Entities
+mkdir $DOMAINNAME/Repositories
